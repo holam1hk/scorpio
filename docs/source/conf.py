7857cd1457e03eb34db5bffa6ddb3bd95a54c8ff
holam1hk <44200076+holam1hk@users.noreply.github.com>
2023-10-10 18:37:55 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,8 +32,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.ifconfig',
               'sphinx.ext.viewcode',
               'sphinx.ext.autosummary',
-              'sphinx_rtd_theme',
-              ]
+              'sphinx_rtd_theme']
 #              
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
