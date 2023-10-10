c851d9b6d43760f929080007a07268b3897955e1
holam1hk <44200076+holam1hk@users.noreply.github.com>
2023-10-10 17:12:14 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,7 @@ numfig = True
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'sphinx-rtd-theme'
+#html_theme = 'sphinx-rtd-theme'
 
 #html_sidebars = {
 #   '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
